02a04f454cf9f20cbc208b9fc1eae99c6f0b6b9f
Aman <raiaman21@gmail.com>
2022-07-10 23:40:03 +0530

    Logical programs

---
+++ sumoffirst.java
@@ -0,0 +1,11 @@
+public class sumoffirst{
+    public static void main(String[] args) {
+        
+        int sum = 0 ;
+        int n = 10;
+        for (int i = 0; i < n; i++){
+        sum = sum + (2*i);
+        }
+        System.out.println(sum);
+    }
+}
